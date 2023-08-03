8c8e259d5d1539a34a9894b28f4e9f5e04f26cf2
Krishsaini2003 <92174891+Krishsaini2003@users.noreply.github.com>
2023-08-03 19:25:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function sendEmail()
+{
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "brokenstorm003@gmail.com",
+    Password : "9868996536",
+    To : 'krishsaini0214@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : "New contact form Enquiry",
+    Body : "Name :"+ document.getElementById("name").value
+          + " <br> Email: "+ document.getElementById("email").value
+          + " <br> subject: "+ document.getElementById("subject").value
+          + " <br> message: "+ document.getElementById("message").value
+}).then(
+  message => alert("message send succesfully")
+);
+}
